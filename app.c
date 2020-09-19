6f8e9d7d83d7ea12cea7804de787821b8800df15
Rafal <rafal@debian.rafal>
2020-09-19 16:49:11 +0200

    working gtktextview

--- app.c
+++ app.c
@@ -4,7 +4,10 @@ static void
 print_hello (GtkWidget *widget,
              gpointer   data)
 {
-  g_print ("Hello World\n");
+  GList* list = gtk_container_get_children(GTK_CONTAINER(widget));
+  GtkLabel* label = ((GtkLabel*) list->data);
+  const gchar* text = gtk_label_get_text(GTK_LABEL(label));
+  g_print("label: %s \n", text);
 }
 
 int
@@ -35,6 +38,10 @@ main (int   argc,
 
   //event_box = gtk_event_box_new ();
   widget = gtk_builder_get_object (builder, "tab0");
+  g_signal_connect (widget, "button-press-event",
+                    G_CALLBACK (print_hello), NULL);
+
+  widget = gtk_builder_get_object (builder, "tab1");
   g_signal_connect (widget, "button-press-event",
                     G_CALLBACK (print_hello), NULL);
 
