b69312358e68b42429c13b473bedcf732bc3d7a0
Rafal <rafal@debian.rafal>
2020-09-19 17:39:50 +0200

    gtkscrolled window

--- app.c
+++ app.c
@@ -31,20 +31,20 @@ main (int   argc,
       g_clear_error (&error);
       return 1;
     }
-
+  
   /* Connect signal handlers to the constructed widgets. */
   window = gtk_builder_get_object (builder, "window");
   g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
-
+  
   //event_box = gtk_event_box_new ();
   widget = gtk_builder_get_object (builder, "tab0");
   g_signal_connect (widget, "button-press-event",
                     G_CALLBACK (print_hello), NULL);
-
+  
   widget = gtk_builder_get_object (builder, "tab1");
   g_signal_connect (widget, "button-press-event",
                     G_CALLBACK (print_hello), NULL);
-
+  
   /*
   button = gtk_builder_get_object (builder, "button1");
   g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
