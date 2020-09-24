8e8a49fcb6c75c3a7ca14502cc74aee86b90cd6f
Rafal <rafal@debian.rafal>
2020-09-24 15:44:31 +0200

    listview working

--- search_path/search_path.c
+++ search_path/search_path.c
@@ -1,10 +1,33 @@
 #include <gtk/gtk.h>
 
-static void
-print_hello (GtkWidget *widget,
-             gpointer   data)
-{
-  g_print ("Hello World\n");
+void
+preedit_changed (GtkWidget *widget, GdkEventKey *event, gpointer userdata)  {
+  g_print("data %s \n", gtk_entry_get_text(GTK_ENTRY(widget)));
+}
+
+void add_to_list(GtkListStore* store, gchar *str0, gchar *str1, gchar *str2) {
+  GtkTreeIter iter;
+  //GType types[10];
+
+  //gtk_list_store_set_column_types(store, 2, types);
+
+  gtk_list_store_append(store, &iter);
+  gtk_list_store_set(store, &iter, 0, str0, 1, str1, 2, str2, -1);
+}
+
+void on_changed(GtkWidget *widget, gpointer userdata) {
+  GtkTreeIter iter;
+  GtkTreeModel *model;
+  gchar *value;
+
+  if (gtk_tree_selection_get_selected(
+      GTK_TREE_SELECTION(widget), &model, &iter)) {
+
+    gtk_tree_model_get(model, &iter, 0, &value,  -1);
+    //gtk_label_set_text(GTK_LABEL(label), value);
+    g_print("value: %s \n", value);
+    g_free(value);
+  }
 }
 
 int
@@ -15,6 +38,7 @@ main (int   argc,
   GObject *window;
   GObject *button;
   GError *error = NULL;
+  GtkTreeSelection *selection; 
 
   gtk_init (&argc, &argv);
 
@@ -27,9 +51,39 @@ main (int   argc,
       return 1;
     }
 
-  /* Connect signal handlers to the constructed widgets. */
+  // Connect signal handlers to the constructed widgets. 
   window = gtk_builder_get_object (builder, "window");
   g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+  
+  button = gtk_builder_get_object (builder, "entry");
+  g_signal_connect (button, "changed", G_CALLBACK (preedit_changed), NULL);
+
+  GtkTreeView *treeview = NULL;
+  GtkTreeModel *model = NULL;
+  GtkListStore *liststore = NULL;
+  GtkTreeIter iter;
+  gchar* data1 = "Hello1aaaaa";
+  gchar* data2 = "Hello2aaaa";
+
+  treeview = GTK_TREE_VIEW(gtk_builder_get_object(builder, "treeview1"));
+  model = gtk_tree_view_get_model(treeview);
+  liststore = GTK_LIST_STORE(model);
+
+  gtk_list_store_append(liststore, &iter);
+  gtk_list_store_set(liststore, &iter, 0, data1, 1, data2, -1);
+
+  //button = gtk_builder_get_object (builder, "listview");
+  //selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(button));
+  //g_signal_connect(selection, "changed", G_CALLBACK(on_changed), NULL);
+
+  //button = gtk_builder_get_object (builder, "liststore");
+  /*
+  add_to_list(GTK_LIST_STORE(button), "Aaa", "aaa", "Aadwd");
+  add_to_list(GTK_LIST_STORE(button), "Aaa", "aaa", "Aadwd");
+  add_to_list(GTK_LIST_STORE(button), "Aaa", "aaa", "Aadwd");
+  add_to_list(GTK_LIST_STORE(button), "Aaa", "aaa", "Aadwd");
+  add_to_list(GTK_LIST_STORE(button), "Aaa", "aaa", "Aadwd");
+  */
   /*
   button = gtk_builder_get_object (builder, "button1");
   g_signal_connect (button, "clicked", G_CALLBACK (print_hello), NULL);
