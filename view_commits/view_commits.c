2638a81dda02840dda3f1a3f7566836e336d10fc
Rafal Braun <rafalbraun92@gmail.com>
2020-10-17 01:14:50 +0200

    counting commits

--- view_commits/view_commits.c
+++ view_commits/view_commits.c
@@ -9,7 +9,6 @@
 #define REPO "/home/rafal/IdeaProjects/gtksourceview-my-ide/application"
 #define SIZE 1024
 
-
 static void
 print_hello (GtkWidget *widget,
              gpointer   data)
@@ -24,6 +23,7 @@ print_hello (GtkWidget *widget,
 // https://stackoverflow.com/questions/17914375/listing-all-commits-in-a-branch-using-libgit2
 // https://libgit2.org/docs/guides/101-samples/
 // https://libgit2.org/libgit2/ex/HEAD/general.html
+// https://www.chromium.org/developers/fast-intro-to-git-internals
 
 static git_commit*
 git_get_head(git_repository *repo) {
@@ -53,7 +53,7 @@ void add_to_list(GtkListStore *liststore, const gchar *str) {
   gtk_list_store_set(liststore, &iter, 0, str, -1);
 }
 
-void visit(git_commit *commit, GtkListStore* liststore)
+void visit(git_commit *commit, GtkListStore* liststore, int* level)
 {
   size_t i, num_parents = git_commit_parentcount(commit);
   gchar buffer[SIZE];
@@ -73,15 +73,15 @@ void visit(git_commit *commit, GtkListStore* liststore)
   cmtter   = git_commit_committer(commit);
   time     = git_commit_time(commit);
 
-  message[strlen(message)-1] = '\0';
+  message[strlen(message)] = '\0';
   s_time = ctime(&time);
   s_time[strlen(s_time)-1] = '\0';
 
   //snprintf(buffer, SIZE, "%s :: %s", oidstr, git_commit_message(c));
-  snprintf(buffer, SIZE, "%s", git_commit_message(commit));
+  //snprintf(buffer, SIZE, "%s", git_commit_message(commit));
   //printf("%s %s\n", message, s_time);
   //printf("%s %s\n", git_commit_message(commit), ctime(&time));
-  snprintf(buffer, SIZE, "%s \n %s", s_time, message);
+  snprintf(buffer, SIZE, "%s \n [HEAD~%d] %s", s_time, *level, message);
   add_to_list (GTK_LIST_STORE(liststore), buffer);
 
 
@@ -92,24 +92,26 @@ void visit(git_commit *commit, GtkListStore* liststore)
     cmtter->name, cmtter->email,
     ctime(&time), message);
   */
+  *level += num_parents;
 
   for (i=0; i<num_parents; i++) {
     /* Do the same for the parents */
     git_commit *p;
     if (!git_commit_parent(&p, commit, i)) {
-      visit(p, liststore);
+      visit(p, liststore, level);
     }
     git_commit_free(p);
   }
 }
 
 // https://ben.straub.cc/2013/10/02/revwalk/
-static void 
+static int 
 git_init(GtkListStore* liststore) {
     git_libgit2_init();
 
     git_repository *repo = NULL;
     int error = git_repository_open(&repo, REPO);
+    int level = 0;
 
     if (error < 0) {
         //const git_error *e = git_error_last();
@@ -119,10 +121,57 @@ git_init(GtkListStore* liststore) {
     }
 
     git_commit * commit = git_get_head(repo);
-    visit (commit, liststore);
+    visit (commit, liststore, &level);
     git_commit_free (commit);
+
+    g_print("level %d \n", level);
+
+
+
+
+    git_object *obj = NULL;
+    error = git_revparse_single(&obj, repo, "HEAD^{tree}");
+    git_tree *tree = (git_tree *)obj;
+
+/*
+    size_t count = git_tree_entrycount(tree);
+    g_print ("count %d \n", count);
+
+    for (int i=0; i<count; i++) {
+        const git_tree_entry *entry = git_tree_entry_byindex(tree, i);
+        //const char *name = git_tree_entry_name(entry);
+        //g_print("entry: %s \n", entry);
+        git_otype objtype = git_tree_entry_type(entry);
+        g_print("%d -> %d \n", i, objtype);
+    }
+
+    g_print("GIT_OBJ_COMMIT:  %d \n", GIT_OBJ_COMMIT);
+    g_print("GIT_OBJ_TREE:    %d \n", GIT_OBJ_TREE);
+*/
+
+
+    git_tree_entry *entry = NULL;
+    error = git_tree_entry_bypath(&entry, tree, "README.md");
+    git_tree_entry_free(entry); /* caller has to free this one */
+
+    const char *name = git_tree_entry_name(entry);
+    g_print("name <%s> \n", name);
+
+
+
+    return level;
 }
 
+/**
+ * git commands:
+ * 
+ * https://stackoverflow.com/questions/424071/how-to-list-all-the-files-in-a-commit
+ * git ls-tree --name-only -r cef053f45f5c0d60244c8f3f4a52f8d76a376dfa
+ * 
+ * https://stackoverflow.com/questions/338436/how-can-i-view-an-old-version-of-a-file-with-git
+ * git show HEAD~4:notebook.c
+ * 
+ */
 int
 main (int argc, char *argv[])
 {
@@ -130,6 +179,7 @@ main (int argc, char *argv[])
   GObject *window;
   GObject *liststore;
   GError *error = NULL;
+  int commits_num;
 
   gtk_init (&argc, &argv);
 
@@ -147,7 +197,7 @@ main (int argc, char *argv[])
   g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
   liststore = gtk_builder_get_object (builder, "liststore");
-  git_init (GTK_LIST_STORE(liststore));
+  commits_num = git_init (GTK_LIST_STORE(liststore));
 
   /*
   button = gtk_builder_get_object (builder, "button1");
