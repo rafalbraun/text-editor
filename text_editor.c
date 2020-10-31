de39b630a39dd541bdb467bb0f373e866f20386c
Rafal Braun <rafalbraun92@gmail.com>
2020-10-31 02:06:01 +0100

    added style

--- text_editor.c
+++ text_editor.c
@@ -191,6 +191,67 @@ file_new_cb () {
     
 }
 
+void list_schemes() {
+    GtkSourceLanguageManager *lm;
+    GtkSourceStyleSchemeManager *sm;
+    GtkSourceStyleScheme *style_scheme = NULL;
+    const gchar * const * schemes;
+
+    sm = gtk_source_style_scheme_manager_get_default ();
+    schemes = gtk_source_style_scheme_manager_get_scheme_ids (sm);
+    g_print ("Available style schemes:\n");
+    while (*schemes != NULL)
+    {
+        const gchar* const *authors;
+        gchar *authors_str = NULL;
+
+        style_scheme = gtk_source_style_scheme_manager_get_scheme (sm, *schemes);
+
+        authors = gtk_source_style_scheme_get_authors (style_scheme);
+        if (authors != NULL)
+            authors_str = g_strjoinv (", ", (gchar **)authors);
+
+        g_print (" - [%s] %s: %s\n",
+             gtk_source_style_scheme_get_id (style_scheme),
+             gtk_source_style_scheme_get_name (style_scheme),
+             gtk_source_style_scheme_get_description (style_scheme) ?
+                gtk_source_style_scheme_get_description (style_scheme) : "");
+
+        if (authors_str) {
+            g_print ("   by %s\n",  authors_str);
+            g_free (authors_str);
+        }
+
+        ++schemes;
+    }
+    g_print("\n");
+}
+
+void set_scheme(GtkSourceBuffer* buffer) {
+    GtkSourceStyleScheme *style_scheme = NULL;
+    GtkSourceStyleSchemeManager *sm;
+    const gchar * const * schemes;
+
+    sm = gtk_source_style_scheme_manager_get_default ();
+    schemes = gtk_source_style_scheme_manager_get_scheme_ids (sm);
+    schemes+=3;
+    style_scheme = gtk_source_style_scheme_manager_get_scheme (sm, *schemes);
+    gtk_source_buffer_set_style_scheme (buffer, style_scheme);
+}
+
+void set_langs_dir(GtkSourceBuffer* buffer) {
+    GtkSourceLanguageManager *manager;
+    const gchar * const * search_path;
+
+    manager = gtk_source_language_manager_get_default ();
+    search_path = gtk_source_language_manager_get_search_path(manager);
+
+    int i=0;
+    while (search_path[i]) {
+        g_print("%s \n", search_path[i++]);
+    }
+
+}
 // https://en.wikibooks.org/wiki/GTK%2B_By_Example/Tree_View/Tree_Models
 int
 main (int argc, char *argv[])
@@ -235,6 +296,16 @@ main (int argc, char *argv[])
     userdata->notebook  = notebook  = gtk_builder_get_object (builder, "notebook");
     userdata->treeview_menu = gtk_builder_get_object (builder, "treeview_context_menu");
     
+
+
+
+
+    set_scheme(GTK_SOURCE_BUFFER(buffer));
+
+
+
+
+
   GtkWidget* action_widget = gtk_button_new_from_icon_name ("list-add-symbolic", GTK_ICON_SIZE_BUTTON);
   g_signal_connect (action_widget, "clicked", G_CALLBACK (print_hello), notebook);
   gtk_notebook_set_action_widget (GTK_NOTEBOOK (notebook), action_widget, GTK_PACK_END);
